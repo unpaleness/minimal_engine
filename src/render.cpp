b46ef54ffefe1b5e026545d1af369e03b30f21e5
unpaleness <generalissimusunpaleness@gmail.com>
2026-04-12 11:30:20 +0200

    small refactor

--- src/render.cpp
+++ src/render.cpp
@@ -1352,10 +1352,10 @@ void Render::RecordCommandBuffer(VkCommandBuffer commandBuffer, const uint32_t i
 
 void Render::UpdateUniformBuffer(uint32_t currentImage)
 {
-    static auto startTime = std::chrono::high_resolution_clock::now();
+    static const auto startTime = std::chrono::high_resolution_clock::now();
 
-    auto currentTime = std::chrono::high_resolution_clock::now();
-    float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+    const auto currentTime = std::chrono::high_resolution_clock::now();
+    const float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
     UniformBufferObject ubo{};
     ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
